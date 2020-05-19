deab573a2c8541f57f0ea490e2d9d59f24139961
Presh Listana <preshlistana@berkeley.edu>
2020-05-18 21:31:57 -0700

    Started first Ruby file

---
+++ ex1.rb
@@ -0,0 +1,48 @@
+# ****** STRINGS AND COMPUTATION ******
+
+# puts "I will now count my chickens:"
+
+# puts "Hens #{25 + 30 / 6}" #Using pound signs is a way to include computations inside strings
+# puts "Roosters #{100 - 25 * 3 % 4}"
+
+# puts "Is it greater? #{5 > -2}"
+# puts "Is it greater or equal? #{5 >= -2}"
+# puts "Is it less or equal? #{5 <= -2}"
+
+# ****** VARIABLES ******
+
+# cars = 100
+# space_in_a_car = 4.0
+# drivers = 30
+# passengers = 90
+# cars_not_driven = cars - drivers
+# cars_driven = drivers
+# carpool_capacity = cars_driven * space_in_a_car
+# average_passengers_per_car = passengers / cars_driven
+
+
+# puts "There are #{cars} cars available."
+# puts "There are only #{drivers} drivers available."
+# puts "There will be #{cars_not_driven} empty cars today."
+# puts "We can transport #{carpool_capacity} people today."
+# puts "We have #{passengers} to carpool today."
+# puts "We need to put about #{average_passengers_per_car} in each car."
+
+# ***** MORE VARIABLES *****
+
+# name = 'Presh'
+# age = 23 
+# height = 60 # inches
+# weight = 110 # lbs
+# eyes = 'Black'
+# teeth = 'White'
+# hair = 'Black'
+
+# puts "Let's talk about #{name}."
+# puts "She's #{height} inches tall."
+# puts "She's #{weight} pounds heavy."
+# puts "Actually that's not too heavy."
+# puts "She's got #{eyes} eyes and #{hair} hair."
+# puts "Her teeth are usually #{teeth} depending on the coffee."
+
+# puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
